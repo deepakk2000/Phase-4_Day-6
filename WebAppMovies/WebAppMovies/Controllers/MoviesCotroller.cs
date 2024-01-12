4a54323f9168d98eacd302a18e38713dd626e81a
deepakk2000 <deepakjc112000@gmal.com>
2024-01-12 16:48:50 +0530

    A-6

---
+++ WebAppMovies/WebAppMovies/Controllers/MoviesCotroller.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAppMovies.Models;
+
+namespace WebAppMovies.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesCotroller : ControllerBase
+    {
+        private static readonly List<Movie> movies = new List<Movie>()
+        {
+            new() {Id=1,MName="Caption Miller",Gen="Action"},
+            new() {Id=2,MName="Ayalan",Gen="Si-Fi"},
+            new() {Id=3,MName="Docker",Gen="Horror"}
+
+        };
+        [HttpGet(Name = "GetMovies")]
+        public IEnumerable<Movie> Get()
+        {
+            return movies;
+        }
+    }
+}
---
+++ WebAppMovies/WebAppMovies/Models/Movie.cs
@@ -0,0 +1,10 @@
+﻿namespace WebAppMovies.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string MName { get; set; }
+        public string Gen { get; set; }
+       
+    }
+}
